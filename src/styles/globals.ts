d8c80faf85c79a6c244b635bdaf03eed087d5ae0
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-07 22:40:01 -0400

    feat: reset css

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0%;
+    padding: 0;
+    box-sizing: border-box;
+}
+
 body{
     background-color: #0F0D13;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
 }
 `

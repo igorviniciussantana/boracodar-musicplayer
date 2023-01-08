5a7c94d70e6f1abb238ec9de6067fde3d2218a0a
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-07 23:46:16 -0400

    feat: setup icons

--- src/components/MusicCard/MusicCard.tsx
+++ src/components/MusicCard/MusicCard.tsx
@@ -3,7 +3,9 @@ import * as Styled from './styled'
 
 import { Roboto } from '@next/font/google'
 
-const roboto = Roboto({weight: ['700', '400'], subsets: ['latin']})
+const robotoBold = Roboto({weight: '700', subsets: ['latin']})
+const robotoRegular = Roboto({weight: '400', subsets: ['latin']})
+
 
 export default function MusicCard(){
     return(
@@ -13,10 +15,22 @@ export default function MusicCard(){
                 <Image src='/image.png' alt='' fill/>
             </Styled.ImageBox>
             <Styled.MusicNames>
-            <Styled.Title className={roboto.className}>Acorda Devinho</Styled.Title>
-            <Styled.Subtitle>Banda Rocketseat</Styled.Subtitle>
+            <Styled.Title className={robotoBold.className}>Acorda Devinho</Styled.Title>
+            <Styled.Subtitle className={robotoRegular.className}>Banda Rocketseat</Styled.Subtitle>
             </Styled.MusicNames>
             </Styled.MusicInfo>
+            <Styled.Icons>
+                <Styled.IconBox>
+                <Image src='/icons/play-back.svg' alt='back-button' fill/>
+                </Styled.IconBox>
+                <Styled.IconBox>
+                <Image src='/icons/play.svg' alt='back-button' fill/>
+                </Styled.IconBox>
+                <Styled.IconBox>
+                <Image src='/icons/play-forward.svg' alt='back-button' fill/>
+                </Styled.IconBox>
+
+            </Styled.Icons>
            
         </Styled.MusicCard>
     )
--- src/components/MusicCard/styled.ts
+++ src/components/MusicCard/styled.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const MusicCard = styled.main`
-
 background-color: #2A2141;
 padding: 3rem;
 border-radius: 0.5rem;
+color: #E1E1E6;
+
 
 
 @media screen and (min-width: 700px){
     width: 50vw;
-    height: 30vw;
     display: flex;
     align-items: center;
     flex-direction: column;
@@ -19,25 +19,53 @@ border-radius: 0.5rem;
 export const MusicInfo = styled.div`
 display: flex;
 width: 100%;
-justify-content: space-between;
+column-gap: 2rem;
 `
 
 export const MusicNames = styled.div`
 display: flex;
+justify-content: center;
 flex-direction: column;
 `
 export const Title = styled.h1`
-
+font-weight: inherit;
+font-size: 3rem;
 `
 
 export const Subtitle = styled.h2`
+opacity: 0.67;
+margin-top: 0.5rem;
+font-size: 2rem;
 
 `
 
 
 export const ImageBox = styled.div`
 position: relative;
-width: 6rem;
+width: 12rem;
 aspect-ratio: 1/1;
-background-color: red;
+
+img{
+border-radius: 0.4rem;
+
+}
+`
+
+export const Icons = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 60%;
+margin-top: 2rem;
+`
+
+export const IconBox = styled.div`
+position: relative;
+width: 4rem;
+aspect-ratio: 1/1;
+
+
+img{
+    
+}
 `

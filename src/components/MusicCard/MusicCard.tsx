84801bc94de02c4565aeb0f28509926d067f6f37
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-09 10:18:57 -0400

    style: auto format MusicCard.tsx

--- src/components/MusicCard/MusicCard.tsx
+++ src/components/MusicCard/MusicCard.tsx
@@ -6,41 +6,44 @@ const robotoBold = Roboto({ weight: "700", subsets: ["latin"] });
 const robotoRegular = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function MusicCard() {
-    const audioRef = useRef() as MutableRefObject<HTMLAudioElement>;
+  const audioRef = useRef() as MutableRefObject<HTMLAudioElement>;
 
-    const [isPlaying, setIsPlaying] = useState(false)
-    const [duration, setDuration] = useState<number | undefined>(0)
-  
-    function setPlayingState(state : any) {
-      setIsPlaying(state)
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState<number | undefined>(0);
+
+  function setPlayingState(state: any) {
+    setIsPlaying(state);
+  }
+  {
+    console.log(audioRef);
+  }
+
+  function toggleIsPlaying() {
+    setIsPlaying(!isPlaying);
+  }
+
+  useEffect(() => {
+    duration == 0
+      ? setDuration(audioRef.current?.duration / 60)
+      : setDuration(duration);
+
+    if (!audioRef.current) {
+      return;
     }
-    {console.log(audioRef)}
 
-    function toggleIsPlaying() {
-      setIsPlaying(!isPlaying)
-    }
-  
-    useEffect(()=> {
-    (duration == 0 ?  setDuration(audioRef.current?.duration / 60) : setDuration(duration))
-
-      if (!audioRef.current) {
-          return;
-      }
-  
-      if (isPlaying) {
-          audioRef.current.play();
-      const timer = setInterval(() => typeof duration == 'number' ? setDuration( (duration) - 0.01 ) : 0, 1000);
+    if (isPlaying) {
+      audioRef.current.play();
+      const timer = setInterval(
+        () => (typeof duration == "number" ? setDuration(duration - 0.01) : 0),
+        1000
+      );
       return () => clearInterval(timer);
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, duration]);
 
-
-      } else {
-          audioRef.current.pause()
-      }
-    }, [isPlaying, duration])
-
-   
-
-      return (
+  return (
     <Styled.MusicCard>
       <Styled.MusicInfo>
         <Styled.ImageBox>
@@ -60,25 +63,37 @@ export default function MusicCard() {
           <Image src="/icons/play-back.svg" alt="back-button" fill />
         </Styled.IconBox>
         <Styled.IconBox>
-          
-          {!isPlaying ? (<Image src="/icons/play.svg" onClick={toggleIsPlaying} alt="back-button" fill />) : (<Image src="/icons/pause.svg" onClick={toggleIsPlaying} alt="back-button" fill />)}
-          
+          {!isPlaying ? (
+            <Image
+              src="/icons/play.svg"
+              onClick={toggleIsPlaying}
+              alt="back-button"
+              fill
+            />
+          ) : (
+            <Image
+              src="/icons/pause.svg"
+              onClick={toggleIsPlaying}
+              alt="back-button"
+              fill
+            />
+          )}
         </Styled.IconBox>
         <Styled.IconBox>
           <Image src="/icons/play-forward.svg" alt="back-button" fill />
         </Styled.IconBox>
       </Styled.Icons>
 
-      <audio 
-            src='https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3' 
-            autoPlay={false} 
-            ref={audioRef}
-            onPlay={() => setPlayingState(true)}
-            onPause={() => setPlayingState(false)}
-        />
-{ typeof duration == 'number' ? (duration).toString().substring(0, 4) : '00:00'}
-       
+      <audio
+        src="https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
+        autoPlay={false}
+        ref={audioRef}
+        onPlay={() => setPlayingState(true)}
+        onPause={() => setPlayingState(false)}
+      />
+      {typeof duration == "number"
+        ? duration.toString().substring(0, 4)
+        : "00:00"}
     </Styled.MusicCard>
-    
   );
 }

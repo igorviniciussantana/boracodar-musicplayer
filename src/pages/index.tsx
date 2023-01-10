f66fd84cbd43a62274461bb85494ac7128d910d7
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-10 16:54:32 -0400

    feat: add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>#BoraCodar | Music Player</title>
         <meta name="description" content="Music Player para o primeiro desafio do projeto Bora Codar da Rocketseat." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
      <main>
 <MusicCard />
